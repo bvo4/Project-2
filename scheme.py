506370b792210d821f21d97d4302733d36a2f9e1
crytin <ac7807@nyu.edu>
2022-04-19 10:45:56 -0400

    Added AES Sample

--- scheme.py
+++ scheme.py
@@ -1,1 +1,12 @@
 from Crypto.Cipher import AES
+
+# AES = https://pycryptodome.readthedocs.io/en/latest/src/cipher/aes.html
+
+key = b'Sixteen byte key'
+cipher = AES.new(key, AES.MODE_EAX)
+data = b'testing1'
+
+nonce = cipher.nonce
+ciphertext, tag = cipher.encrypt_and_digest(data)
+print(data)
+print(ciphertext)
