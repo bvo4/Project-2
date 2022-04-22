554ab4638120d534ee9789995b7b45db26fd3597
bvo4 <55567877+bvo4@users.noreply.github.com>
2022-04-22 12:48:54 -0400

    Update Threefish.py

--- Threefish.py
+++ Threefish.py
@@ -1,7 +1,22 @@
+import sys
+
+#https://www.geeksforgeeks.org/decision-tree-implementation-python/
 from skein import threefish
 
-t = threefish(b'key of 32,64 or 128 bytes length', b'tweak: 16 bytes ')
-(32, 256)
- c= t.encrypt_block(b'block of data,same length as key')
-print (t.decrypt_block(c))
-print('block of data,same length as key')
+def _tf_encrypt(key, iv, data):
+	# adso always uses JSON, so we pad the message with JSON whitespace.
+	while len(data) % 64 != 0:
+		data += b' '
+	cipher = skein.threefish(derive_key(key, iv, 1024), _tf_tweak_ctr(0))
+	output = b''
+	for k in range(0, len(data), 64):
+		cipher.tweak = _tf_tweak_ctr(k) 
+		output += cipher.encrypt_block(data[k : k + 64])
+	return output
+    
+
+
+if __name__ == "__main__":
+    key = 'B&E(H+MbQeThWmZq4t7w!z%C*F-JaNcRfUjXn2r5u8x/A?D(G+KbPeSgVkYp3s6v9y$B&E)H@McQfTjWmZq4t7w!z%C*F-JaNdRgUkXp2r5u8x/A?D(G+KbPeShVmYq'
+
+    _tf_encrypt(key, 4, b'ffhfdfdjfjkfhjhfjadhjfkjkfldahjfdahkfhajfkd')
