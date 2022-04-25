0e18f911413fe0e9f14593cffb149da2778fdc52
bvo4 <brandonvo1980@yahoo.com>
2022-04-25 13:00:55 -0400

    Adding a web interface for later

---
+++ webinterface/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//https://blog.bitsrc.io/create-react-app-without-create-react-app-b0a5806a92
+
+const App = () =>{
+    return (
+        <h1>
+            CS 6903 project:  Decision Tree interface for symmetric block cipher primitiives
+        </h1>
+    )
+}
+
+export default App
